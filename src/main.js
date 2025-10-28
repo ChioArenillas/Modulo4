0ff5c3c7c392365fb0d9e2081f228d0ab7d44158
chioarenillas <arenillasr@gmail.com>
2025-10-28 12:57:28 +0100

    changes

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 
-/* Según cuál queramos mostrar comentamos uno u otro */
+/* Para ver la práctica 1 y 2 tenemos que comentar la línea 11 (práctica 3y4), 
+para ver la práctica 3 y 4 tenemos que comentar la línea 10 y descomentar la 11 */
 
 //STYLES
 import "../src/css/headerYfooter.css";
 import "../src/css/style.css";
 
 //JAVASCRIPT
-//import "./practica1y2.js";
-import "./practica3y4.js";
-
+import "./practica1y2.js";
+//import "./practica3y4.js";
